7bb0199e006a9fc931adfc80fd4bb91c43749613
wangjiayi <wjiayi01@gmail.com>
2018-01-18 01:55:31 -0800

    new

--- sketch.js
+++ sketch.js
@@ -1,32 +1,37 @@
 
-function setup(){
-  var myCanvas = createCanvas(window.innerWidth, 400);
-  myCanvas.parent('mySketch');
-  background(155);
-}
-function draw(){
-  stroke(50);
-  fill(0);
-  for(var i = 50; i < 400; i += 50){
-    ellipse(i,i,50,50);
-  }
-  if(mouseIsPressed){
-    fill(255);
-    ellipse(mouseX, mouseY, 80, 80);
-  }
+// function setup(){
+//   var myCanvas = createCanvas(window.innerWidth, 200);
+//   myCanvas.parent('mySketch');
+//   background(155);
+// }
 
-  // x = x + random(-1,-1);
-  // y = y -1;
-  // if(y < 0){
-  //   y = height;
-  // }
-  // if(mouseIsPressed){
-  //   fill(255);
-  //   ellipse(mouseX, mouseY, 80, 80);
-  // }else{
-  //   fill(0);
-  //   stroke(color(0, 0, 255));
-  //   strokeWeight(4);
-  //   rect(mouseX, mouseY, 60, 60);
-  // }
+// function draw(){
+//   stroke(50);
+//   fill(0);
+//   for(var i = 50; i < 400; i += 50){
+//      makeParticles(2, touchX, touchY);
+//   }
+//   if(mouseIsPressed){
+//     fill(255);
+//     makeParticles(2, touchX, touchY);
+//   }
+// }
+
+function setup() {
+    var myCanvas = createCanvas(800, 250);
+    myCanvas.parent('mySketch');   
+}
+function draw() {  
+    var Color_1 = random(255);
+    var Color_2 = random(255);
+    var Color_3 = random(255);
+    var alphaColor = random(500); 
+    var s = random(20);
+    fill(Color_1,Color_2,Color_3,alphaColor);
+    rect(mouseX, mouseY, s, s);
+    if(mouseIsPressed){
+      fill(Color_1,Color_2,Color_3,alphaColor);
+      ellipse(mouseX, mouseY, s+20, s+20);
+    }
+    
 }
